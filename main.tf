8b23c96ed9df61154d2d03de59296ab692bc8909
Sean Hellum <seanhellum45@gmail.com>
2020-03-30 22:27:18 +0000

    make commands with provisoners

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "HelloTerraform" {
   ami           = "ami-0fc20dd1da406780b"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+      command = "echo Hello World From Terraform > helloworld.txt"
+  }
 }
