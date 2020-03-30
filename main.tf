1d83dd343415991d98bd9e98d28ac644bb73c55b
Sean Hellum <seanhellum45@gmail.com>
2020-03-30 23:13:01 +0000

    format code

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "HelloTerraform" {
   ami           = "ami-0fc20dd1da406780b"
   instance_type = "t2.micro"
   provisioner "local-exec" {
-      command = "echo Hello World From Terraform > helloworld.txt"
+    command = "echo Hello World From Terraform > helloworld.txt"
   }
 }
