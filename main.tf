0b8989687e2393105076912d93868d593dcc91b6
Sean Hellum <seanhellum45@gmail.com>
2020-03-30 17:27:34 +0000

    format terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    profile = "default"
-    region = "us-east-2"
+  profile = "default"
+  region  = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0fc20dd1da406780b"
-    instance_type = "t2.micro"
+  ami           = "ami-0fc20dd1da406780b"
+  instance_type = "t2.micro"
 }
